0f359de5a85b2d9d5978163076e96bf4f842345a
Abijah Kabiro <abijahkabiro@gmail.com>
2020-08-03 19:01:14 +0300

    add a get input function to get the input and give output

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ var year;
 var month;
 var day;
 var dayOfWeek;
+
+function getInput(){
+    century=parseInt(document.getElementById("century").value);
+    year=parseInt(document.getElementById("year").value);
+    month=parseInt(document.getElementById("month").value);
+    day=parseInt(document.getElementById("day").value);
+    console.log(century);
+    console.log(year);
+    console.log(month);
+    console.log(day);
+}
